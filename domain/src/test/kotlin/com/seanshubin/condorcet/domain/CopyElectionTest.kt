f99cfec2d3a45894130236b9c477e722bb38a08d
SeanShubin <seanshubin@gmail.com>
2019-06-13 18:56:42 -0700

    copy election when election not found

--- domain/src/test/kotlin/com/seanshubin/condorcet/domain/CopyElectionTest.kt
+++ domain/src/test/kotlin/com/seanshubin/condorcet/domain/CopyElectionTest.kt
@@ -46,34 +46,29 @@ class CopyElectionTest {
         // given
         val api = createEmpty()
         val aliceCredentials = Credentials("Alice", "password")
-        val bobCredentials = Credentials("Bob", "password")
         api.register(aliceCredentials.userName, "alice@email.com", aliceCredentials.userPassword)
-        api.register("Bob", "bob@email.com", "password")
-        api.register("Carol", "carol@email.com", "password")
-        api.register("Dave", "dave@email.com", "password")
         api.createElection(aliceCredentials, "Ice Cream")
-        api.updateCandidateNames(aliceCredentials, "Ice Cream", listOf("Chocolate", "Vanilla", "Strawberry"))
-        api.setEndDate(aliceCredentials, "Ice Cream", "2019-06-11T22:57:26.497Z")
-        api.setSecretBallot(aliceCredentials, "Ice Cream", secretBallot = false)
-        api.updateEligibleVoters(aliceCredentials, "Ice Cream", listOf("Alice", "Carol"))
-        api.doneEditingElection(aliceCredentials, "Ice Cream")
 
         api.createElection(aliceCredentials, "Already Exists")
 
-        val expected = ElectionDetail(
-                ownerName = "Bob",
-                name = "Copied Election",
-                endIsoString = null,
-                secretBallot = false,
-                status = ElectionStatus.EDITING,
-                candidateNames = listOf("Chocolate", "Vanilla", "Strawberry"),
-                voterNames = listOf("Alice", "Carol"),
-                isAllVoters = false)
-
         // when
-        val result = Try { api.copyElection(bobCredentials, "Already Exists", "Ice Cream") }
+        val result = Try { api.copyElection(aliceCredentials, "Already Exists", "Ice Cream") }
 
         // then
         assertEquals("Election with name 'Already Exists' already exists", (result as Failure).exception.message)
+    }
+
+    @Test
+    fun electionToCopyDoesNotExist() {
+        // given
+        val api = createEmpty()
+        val aliceCredentials = Credentials("Alice", "password")
+        api.register(aliceCredentials.userName, "alice@email.com", aliceCredentials.userPassword)
+
+        // when
+        val result = Try { api.copyElection(aliceCredentials, "Copied Election", "Ice Cream") }
+
+        // then
+        assertEquals("election 'Ice Cream' not found", (result as Failure).exception.message)
     }
 }
