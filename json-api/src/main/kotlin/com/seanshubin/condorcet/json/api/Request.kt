b425e6d47f7d297a0bc72448bc87f871d1bafac2
Sean Shubin <sshubin@cj.com>
2019-08-01 15:03:54 -0700

    Removed response class

--- json-api/src/main/kotlin/com/seanshubin/condorcet/json/api/Request.kt
+++ json-api/src/main/kotlin/com/seanshubin/condorcet/json/api/Request.kt
@@ -4,11 +4,10 @@ import com.fasterxml.jackson.module.kotlin.readValue
 import com.seanshubin.condorcet.domain.Api
 import com.seanshubin.condorcet.domain.Credentials
 import com.seanshubin.condorcet.json.JsonUtil
-import com.seanshubin.condorcet.json.api.Response.*
 import java.time.Instant
 
 interface Request {
-    fun exec(api: Api): Response
+    fun exec(api: Api): Any
 
     companion object {
         fun parse(name: String, json: String): Request =
@@ -35,71 +34,71 @@ interface Request {
     }
 
     data class LoginRequest(val nameOrEmail: String, val password: String) : Request {
-        override fun exec(api: Api): Response = LoginResponse(api.login(nameOrEmail, password))
+        override fun exec(api: Api): Any = api.login(nameOrEmail, password)
     }
 
     data class RegisterRequest(val name: String, val email: String, val password: String) : Request {
-        override fun exec(api: Api): Response = RegisterResponse(api.register(name, email, password))
+        override fun exec(api: Api): Any = api.register(name, email, password)
     }
 
     data class CreateElectionRequest(val credentials: Credentials, val electionName: String) : Request {
-        override fun exec(api: Api): Response = CreateElectionResponse(api.createElection(credentials, electionName))
+        override fun exec(api: Api): Any = api.createElection(credentials, electionName)
     }
 
     data class SetEndDateRequest(val credentials: Credentials, val electionName: String, val endDate: Instant?) : Request {
-        override fun exec(api: Api): Response = SetEndDateResponse(api.setEndDate(credentials, electionName, endDate))
+        override fun exec(api: Api): Any = api.setEndDate(credentials, electionName, endDate)
     }
 
     data class SetSecretBallotRequest(val credentials: Credentials, val electionName: String, val secretBallot: Boolean) : Request {
-        override fun exec(api: Api): Response = SetSecretBallotResponse(api.setSecretBallot(credentials, electionName, secretBallot))
+        override fun exec(api: Api): Any = api.setSecretBallot(credentials, electionName, secretBallot)
     }
 
     data class DoneEditingElectionRequest(val credentials: Credentials, val electionName: String) : Request {
-        override fun exec(api: Api): Response = DoneEditingElectionResponse(api.doneEditingElection(credentials, electionName))
+        override fun exec(api: Api): Any = api.doneEditingElection(credentials, electionName)
     }
 
     data class EndElectionRequest(val credentials: Credentials, val electionName: String) : Request {
-        override fun exec(api: Api): Response = EndElectionResponse(api.endElection(credentials, electionName))
+        override fun exec(api: Api): Any = api.endElection(credentials, electionName)
     }
 
     data class SetCandidateNamesRequest(val credentials: Credentials, val electionName: String, val candidateNames: List<String>) : Request {
-        override fun exec(api: Api): Response = SetCandidateNamesResponse(api.setCandidateNames(credentials, electionName, candidateNames))
+        override fun exec(api: Api): Any = api.setCandidateNames(credentials, electionName, candidateNames)
     }
 
     data class SetVotersRequest(val credentials: Credentials, val electionName: String, val eligibleVoterNames: List<String>) : Request {
-        override fun exec(api: Api): Response = SetVotersResponse(api.setVoters(credentials, electionName, eligibleVoterNames))
+        override fun exec(api: Api): Any = api.setVoters(credentials, electionName, eligibleVoterNames)
     }
 
     data class SetVotersToAllRequest(val credentials: Credentials, val electionName: String) : Request {
-        override fun exec(api: Api): Response = SetVotersToAllResponse(api.setVotersToAll(credentials, electionName))
+        override fun exec(api: Api): Any = api.setVotersToAll(credentials, electionName)
     }
 
     data class ListElectionsRequest(val credentials: Credentials) : Request {
-        override fun exec(api: Api): Response = ListElectionsResponse(api.listElections(credentials))
+        override fun exec(api: Api): Any = api.listElections(credentials)
     }
 
     data class GetElectionRequest(val credentials: Credentials, val electionName: String) : Request {
-        override fun exec(api: Api): Response = GetElectionResponse(api.getElection(credentials, electionName))
+        override fun exec(api: Api): Any = api.getElection(credentials, electionName)
     }
 
     data class CopyElectionRequest(val credentials: Credentials, val newElectionName: String, val electionToCopyName: String) : Request {
-        override fun exec(api: Api): Response = CopyElectionResponse(api.copyElection(credentials, newElectionName, electionToCopyName))
+        override fun exec(api: Api): Any = api.copyElection(credentials, newElectionName, electionToCopyName)
     }
 
     data class ListBallotsRequest(val credentials: Credentials, val voterName: String) : Request {
-        override fun exec(api: Api): Response = ListBallotsResponse(api.listBallots(credentials, voterName))
+        override fun exec(api: Api): Any = api.listBallots(credentials, voterName)
     }
 
     data class GetBallotRequest(val credentials: Credentials, val electionName: String, val voterName: String) : Request {
-        override fun exec(api: Api): Response = GetBallotResponse(api.getBallot(credentials, electionName, voterName))
+        override fun exec(api: Api): Any = api.getBallot(credentials, electionName, voterName)
     }
 
     data class CastBallotRequest(val credentials: Credentials, val electionName: String, val voterName: String, val rankings: Map<String, Int>) : Request {
-        override fun exec(api: Api): Response = CastBallotResponse(api.castBallot(credentials, electionName, voterName, rankings))
+        override fun exec(api: Api): Any = api.castBallot(credentials, electionName, voterName, rankings)
     }
 
     data class TallyRequest(val credentials: Credentials, val electionName: String) : Request {
-        override fun exec(api: Api): Response = TallyResponse(api.tally(credentials, electionName))
+        override fun exec(api: Api): Any = api.tally(credentials, electionName)
     }
 
 }
